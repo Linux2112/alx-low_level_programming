67ade91abc10e5c09ee3dccd4ce892fe24d2f266
root <linasileshi999@gmail.com>
2023-02-17 16:15:10 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Displays numberswith base 10
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int n = 0;
+
+while (n < 10)
+{
+	printf("%i", n);
+	n++;
+}
+printf("\n");
+return (0);
+}
