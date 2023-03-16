0c936f05eb0c9f8712a4c1c03fac79b9da4c86e6
linux2112 <linasileshi999@gmail.com>
2023-03-16 12:08:47 -0700

    2nd commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,52 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stddef.h>
-
-char *_memset(char *s, char b, unsigned int n);
 
 /**
- * _calloc - allocates memory to an array of nmemb elements of size size
- * @nmemb: elements in the array
- * @size: size of element in the array
- *
- * Return: a pointer to the newly allocated memory
- */
-
-void *_calloc(unsigned int nmemb, unsigned int size)
+  * string_nconcat - ...
+  * @s1: ...
+  * @s2: ...
+  * @n: ...
+  *
+  * Return: ...
+  */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
 
-	if (nmemb == 0 || size == 0)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[i])
+		i++;
+
+	while (s2[k])
+		k++;
+
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
 		return (NULL);
 
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-		return (NULL);
-
-	_memset(ptr, 0, nmemb * size);
-
-	return (ptr);
-}
-
-/**
- * _memset - fills memory with a constant byte, b
- * @s: the memory area to be filled
- * @b: the constant byte that will fill the memory
- * @n: the amount of bytes in memory area s will be filled with b
- *
- * Return: a point to memory s
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int index;
-
-	for (index = 0; index < n; index++)
+	k = 0;
+	while (j < l)
 	{
-		s[index] = b;
+		if (j <= i)
+			str[j] = s1[j];
+
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
 	}
-	return (s);
+	str[j] = '\0';
+	return (str);
 }
